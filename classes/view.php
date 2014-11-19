95554621ada67de24f6fa918a381f168302a732c
Christian Dieckhöner <dieckhoenerc@gmail.com>
2014-11-19 12:38:55 +0100

    Update view.php

--- classes/view.php
+++ classes/view.php
@@ -1,35 +1,43 @@
 <?php
-	class View 
-	{
-		private $data;
-		private $template;
+	class View {
+		private $daten;
+		private $template='default';
 		
-		function __construct ($data, $template = "default") {
-			$this->data 	= 	  $data;
+		public function __construct ($daten='',$template='default') {
+			$this->daten = $daten;
 			$this->template = $template;
 		}
 		
-		function loadTemplate()
-		{
-			$tpl 	= 										$this->template;
-			$file 	= $_SERVER["DOCUMENT_ROOT"]."/mvc/templates/{$tpl}.php";
-			$exists = 		 							 file_exists($file);
-			
-			if($exists)
-			{
+		public function setDaten($daten) {
+			$this->daten=$daten;
+		}
+		
+		public function setTemplate($template) {
+			$this->template=$template;
+		}
+		
+		public function loadTemplate() {
+			$tpl = $this->template; // default
+			$file = './templates/'.$tpl.'.php';
+			if(file_exists($file)) {
 				ob_start();
-				
-				include($file);
-				$output = ob_get_contents();
-				
-				ob_end_clean();
-				
-				return $output;
+				include $file; // Template wird in Puffer geladen
+				$output = ob_get_contents(); // Puffer wird gelesen
+				ob_end_clean(); // Puffer wird gelöscht
+				return $output; // Rückgabe
 			}
 			else
-			{
-				return false;
-			}
+				return false; // Template nicht gefunden
 		}
+		
+		/*	public function show() {
+			// Template wird ausgegeben
+			$str = "<table>";
+			foreach($this->daten as $key=>$value) {
+				$str .= "<tr><td>$key</td><td>$value</td></tr>";
+			}
+			$str .= "</table>";
+			return $str;
+		}*/
 	}
 ?>
